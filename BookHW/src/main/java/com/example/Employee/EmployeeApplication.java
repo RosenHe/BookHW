3ffbca3d1493e3ee82bef41ab34c3663324b2913
Rosen He <rosen33bi@gmail.com>
2022-07-19 16:13:56 -0400

    update July 19Hw

--- BookHW/src/main/java/com/example/Employee/EmployeeApplication.java
+++ BookHW/src/main/java/com/example/Employee/EmployeeApplication.java
@@ -15,23 +15,11 @@ import java.util.Collections;
 @SpringBootApplication
 public class EmployeeApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(com.example.Employee.EmployeeApplication.class, args);
     }
     @Bean(name = "restTemplate")
     public RestTemplate getRestClient() {
-        RestTemplate restClient = new RestTemplate(
-                new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
-
-        // Add one interceptor like in your example, except using anonymous class.
-        restClient.setInterceptors(Collections.singletonList((request, body, execution) -> {
-
-            LOGGER.debug("Intercepting...");
-            return execution.execute(request, body);
-        }));
-
-        return restClient;
+        return new RestTemplate();
     }
 }
